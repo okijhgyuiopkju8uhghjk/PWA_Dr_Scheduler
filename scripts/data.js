94975efdacf67a925e78bd944e471d43fa8bd043
okijhgyuiopkju8uhghjk <994984@gmail.com>
2026-02-06 10:15:53 +0530

    added nikhilesh

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,7 @@
 const doctors = [
   { name: 'Dr Deepak', color: '#E8EB25', textColor: '#2825EB' },
   { name: 'Dr Srinivas', color: '#4CAF50', textColor: '#FFFFFF' },
-  { name: 'Dr Usharani', color: '#2196F3', textColor: '#FFFFFF' },
+  { name: 'Dr Nikhilesh', color: '#2196F3', textColor: '#FFFFFF' },
   { name: 'Dr Rajesh', color: '#f44336', textColor: '#FFFFFF' },
   { name: ' - ', color: '#9E9E9E', textColor: '#FFFFFF' }
 ];
